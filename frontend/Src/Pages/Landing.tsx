58c546d69b0b3a95c1ca91fa966bedb009c40449
azizios011 <hdriaziz699@gmail.com>
2025-05-05 23:31:18 +0100

    V 1.0.0

---
+++ frontend/Src/Pages/Landing.tsx
@@ -0,0 +1,18 @@
+import Header from "../Components/Header"
+import Hero from "../Components/Hero"
+
+const Landing = () => {
+  return (
+    <div>
+        <div>
+            <Header />
+        </div>
+
+        <div>
+            <Hero />
+        </div>
+    </div>
+  )
+}
+
+export default Landing
---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Landing from "@/Src/Pages/Landing";
+
+export default function Home() {
+  return (
+    <div>
+      <Landing />
+    </div>
+  );
+}
